0038bc14ad6106df740aae6b87003730f5a3a591
speed-rmn <speed@rmn.com>
2016-10-07 14:40:24 -0500

    Prevented accidental global var

--- help-widget-script.js
+++ help-widget-script.js
@@ -1,6 +1,6 @@
 $(function () {
-  $dt = $(".faq dt").on("click", function () {
-  	// Close other expanded questions
+  var $dt = $(".faq dt").on("click", function () {
+    // Close other expanded questions
     $dt.filter(".expanded").not(this).removeClass("expanded");
 
     // Toggle whether this question is expanded or not
