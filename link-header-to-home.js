9c2ce3c893d8639a7bc7186641aeea03fe331a25
speed-rmn <speed@rmn.com>
2016-11-10 10:42:05 -0600

    Use page protocol for default URL
    Removed the 'https:' from the default URL so that the <a> will use the current page's protocol.  It's a small optimization.

--- link-header-to-home.js
+++ link-header-to-home.js
@@ -12,7 +12,7 @@
   var homeURL = $home.attr("href");
 
   // default if URL unavailable for whatever reason
-  homeURL = homeURL || "https://gm1.geolearning.com/geonext/retailmenot/displaywidgetpage.geo?id=nQAMXpGBScF8onfKFvzZsWrG92z3eAz1cl3LqNrRDfniuyQEURkFNg..&nav=RMNHomePage";
+  homeURL = homeURL || "//gm1.geolearning.com/geonext/retailmenot/displaywidgetpage.geo?id=nQAMXpGBScF8onfKFvzZsWrG92z3eAz1cl3LqNrRDfniuyQEURkFNg..&nav=RMNHomePage";
 
   // make header image link back to home; hilariously, the header image is already in an <a> tag, it just has no 'href'.  What a system.
   $(".brandimage").find("a").attr("href", homeURL);
